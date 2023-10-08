1ab5362ffbc7061fe00f2e1e3d74382c51ef7fc3
Jack Wickham <jackwickham@live.co.uk>
2023-10-08 21:45:38 +0100

    Use relative paths by default

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,5 @@
 FROM debian:latest
 COPY ./target/release/severe-delays /app/severe-delays
+EXPOSE 8000
 WORKDIR /app
 CMD ["/app/severe-delays"]
--- fe/src/api/api.ts
+++ fe/src/api/api.ts
@@ -1,7 +1,8 @@
 import {ApiResponse} from "./types";
 
 export const loadStatuses = async (from: Date, to: Date): Promise<ApiResponse> => {
-  const response = await fetch(`http://localhost:8000/api/v1/history?from=${from.toISOString()}&to=${to.toISOString()}`);
+  const base = localStorage.getItem("apiBaseUri") || "";
+  const response = await fetch(`${base}/api/v1/history?from=${from.toISOString()}&to=${to.toISOString()}`);
   const data = await response.json();
   return data;
 };
